db41df1456c92e098dc2de01002d2729aa673baa
gregvx <32096447+gregvx@users.noreply.github.com>
2018-12-30 00:17:30 -0700

    stubbing out file structure.

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ('Bacon Cheese', true);
+
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ('Hawaiian', false);
+
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ('Double Double', true);
